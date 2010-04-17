7da5dba7f88eba66c65a4f8fef06c600edbea8e7
krick <antipin.konstantin@googlemail.com>
2010-04-17 09:36:27 +0400

    Робокассный контроллер вроде работает

--- app/controllers/robo_kassa_controller.rb
+++ app/controllers/robo_kassa_controller.rb
@@ -16,8 +16,8 @@ class RoboKassaController < Spree::BaseController
   end
 
   def success
-    load_order_by_number('R' + params[:InvId])
     if @robo_kassa.success?(params)
+      load_order_by_number('R' + params[:InvId].to_s)
       payment = Payment.new(:amount => params[:OutSum])
       payment.order = @order
       payment.save
@@ -25,13 +25,13 @@ class RoboKassaController < Spree::BaseController
     else
       flash[:error] = 'Платёж не прошёл проверку подписи.'
     end
-    redirect_to order_url(@order.number)
+    redirect_to @order ? order_url(@order.number) : root_url
   end
   
   def fail
     load_order_by_number('R' + params[:InvId])
     flash[:error] = 'Платёж не завершен или отменён.'
-    redirect_to order_url(@order.number)
+    redirect_to @order ? order_url(@order.number) : root_url
   end
 
   private 
@@ -42,14 +42,25 @@ class RoboKassaController < Spree::BaseController
   
   def load_order_by_number(number)
     @order = Order.find_by_number(number)
-    unless @order
-      flash[:error] = "Заказ с номером #{number} не найден."
-      redirect_to root_url
-    end
+    # При редиректе мне выдавалась ошибка:
+    # Please note that you may only call render at most once per action. Also note that neither redirect nor render terminate
+    # execution of the action, so if you want to exit an action after redirecting, you need
+    # to do something like "redirect_to(...) and return".)
+
+    # flash[:error] = "Заказ с номером #{number} не найден."
+    # redirect_to root_url
   end
   
   def load_robokassa
-    @robo_kassa = Gateway.current.provider 
+    if not Gateway.current 
+      render :text => "Ошибка конфигурации: Не выбран текущий платежный гейт.",
+      :status => '500'
+    elsif Gateway.current.method_type != 'robo_kassa'
+      render :text => "Ошибка конфигурации: Текущий гейт должен быть 'робокасса'. Сейчас #{Gateway.current.type}",
+      :status => '500'
+    else
+      @robo_kassa = Gateway.current.provider 
+    end
   end
 
 end
